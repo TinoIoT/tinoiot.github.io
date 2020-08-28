38f78a01ad4bf86ff2ac9f2319a80c2e42d80965
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2020-08-28 00:26:12 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,14 +27,14 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-423ccf66b5eedaa35e10.js"
+    "url": "webpack-runtime-ab9edd4617113438b6ef.js"
   },
   {
     "url": "app-95986a59372784db7989.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6a86ca48a4adeb17706821f616fb72d5"
+    "revision": "9666f9d9d93b542616abd7c684919fd1"
   },
   {
     "url": "google-fonts/s/roboto/v20/KFOlCnqEu92Fr1MmWUlfBBc4.woff2",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "10ef30736eb4bfc652849c0e08a2c412"
+    "revision": "2b19ad970d8658c8bbe0342b72b6e76e"
   },
   {
     "url": "polyfill-6eef2b5200577da5f9c2.js"
